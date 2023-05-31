2cc676fef3be174178b77fe7a234532d5dc1dd4e
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-05-31 21:44:17 +0800

    feat: support style hot update

---
+++ packages/vite-plugin/src/hot-update.ts
@@ -0,0 +1,28 @@
+import type { HmrContext, ModuleNode } from 'vite'
+import { parseQuery } from './parse-query'
+
+export function handleHotUpdate(
+  { modules }: HmrContext,
+): ModuleNode[] {
+  const affectedModules = new Set<ModuleNode | undefined>()
+  modules.forEach((m) => {
+    const importedModules = m.importedModules
+    if (importedModules.size > 0) {
+      [...importedModules].forEach((im) => {
+        const { query } = parseQuery(im.id as string)
+        console.log(query.type)
+        // filter css modules
+        if (query.type === 'vine-style') {
+          affectedModules.add(im)
+        }
+      })
+    }
+  })
+
+  if (affectedModules && affectedModules.size > 0) {
+    return [...modules, ...[...affectedModules].filter(Boolean) as ModuleNode[]]
+  }
+  else {
+    return [...modules]
+  }
+}
