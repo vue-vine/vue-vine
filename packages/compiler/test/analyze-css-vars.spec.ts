0b46ace70ef8c046d41a0b98538b0fd4496b2785
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-06-09 00:09:40 +0800

    feat: support v-bind cssvars (#18)
    ---------
    Co-authored-by: ShenQingchuan <tmy991106@gmail.com>

---
+++ packages/compiler/test/analyze-css-vars.spec.ts
@@ -0,0 +1,140 @@
+import { describe, expect, test } from 'vitest'
+import { parseCssVars } from '../src/style/analyze-css-vars'
+
+describe('analyze css vbind', () => {
+  test('Should be able to parse to extract the v-bind value', () => {
+    const source = `
+    .test {
+      color: v-bind(color);
+    }
+  `
+    const expected = ['color']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse single quoted values', () => {
+    const source = `
+    .test {
+      color: v-bind('color');
+    }
+  `
+    const expected = ['color']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse double quoted values', () => {
+    const source = `
+    .test {
+      color: v-bind("color");
+    }
+  `
+    const expected = ['color']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse the value of the template string', () => {
+    const source = `
+    .test {
+      color: v-bind(\`\${v}\`\);
+      background-image: v-bind('\`url('\${bgUrl}'\)\`');
+    }
+  `
+    // eslint-disable-next-line no-template-curly-in-string
+    const expected = ['`${v}`', '`url(\'${bgUrl}\')`']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse extract v-bind values in nested', () => {
+    const source = `
+    .parent {
+      .child {
+        color: v-bind(color);
+      }
+    }
+  `
+    const expected = ['color']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse extract v-bind values when ignoring single line comments', () => {
+    const source = `
+    .test {
+      color: v-bind(color); // this is a comment
+    }
+  `
+    const expected = ['color']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse extract v-bind values when ignoring multi-line comments', () => {
+    const source = `
+    .test {
+      color: v-bind(color); /* this is a
+      multi-line
+      comment */
+    }
+  `
+    const expected = ['color']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to extract multiple v-bind values in analysis', () => {
+    const source = `
+    .test {
+      color: v-bind(color1);
+      background-color: v-bind(color2);
+    }
+  `
+    const expected = ['color1', 'color2']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should only analyze to extract unique values', () => {
+    const source = `
+    .test {
+      color: v-bind(color1);
+      background-color: v-bind(color1);
+    }
+  `
+    const expected = ['color1']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('Should be able to parse to extract values inside nested parentheses', () => {
+    const source = `
+    .test {
+      color: v-bind(((color1)));
+    }
+  `
+    const expected = ['((color1))']
+    expect(parseCssVars([source])).toMatchObject(expected)
+  })
+
+  test('the right parenthesis is missing', () => {
+    const source = `
+    .test {
+      v-bind(color1;
+    }
+  `
+    expect(parseCssVars([source])).toMatchObject([])
+  })
+
+  test('the left parenthesis is missing', () => {
+    const source = `
+    .test {
+      v-bind color1);
+    }
+  `
+    expect(parseCssVars([source])).toMatchObject([])
+  })
+
+  test('should be able to parse incomplete expressions', () => {
+    const source = `
+    .test {
+       font-weight: v-bind("count.toString(");
+       font-weight: v-bind(xxx);
+    }
+  `
+    expect(parseCssVars([source])).toMatchObject(['count.toString(', 'xxx'])
+  })
+})
---
+++ packages/compiler/test/css-vars.spec.ts
@@ -0,0 +1,121 @@
+import { beforeEach, describe, expect, test } from 'vitest'
+import hashId from 'hash-sum'
+import type { VineCompilerHooks } from '../index'
+import { compileVineTypeScriptFile, createCompilerCtx } from '../index'
+import type { VineCompilerCtx } from '../src/types'
+
+let mockVCF = ''
+let mockCompilerHook = {} as VineCompilerHooks
+let mockCompilerCtx = {} as VineCompilerCtx
+beforeEach(() => {
+  mockVCF = 'function testVCF() {\n'
+    + '  const color = ref(\'red\')\n'
+    + '\n'
+    + '  vineStyle.scoped(`\n'
+    + '    .blog-title {\n'
+    + '      color: v-bind(color)\n'
+    + '    }\n'
+    + '  `)\n'
+    + '  return vine`\n'
+    + '     <div class="title">\n'
+    + '        title\n'
+    + '     </div>\n'
+    + '  `\n'
+    + '}'
+
+  mockCompilerCtx = createCompilerCtx({})
+
+  mockCompilerHook = {
+    onError: () => {},
+    onWarn: () => {},
+    onBindFileCtx: (fileId, fileCtx) => mockCompilerCtx.fileCtxMap.set(fileId, fileCtx),
+  } as VineCompilerHooks
+})
+describe('CSS vars injection', () => {
+  test('Should be injected based on reactive variables', () => {
+    const res = compileVineTypeScriptFile(mockVCF, 'mockVCF', mockCompilerHook)
+    const code = res.fileSourceCode.toString()
+    expect(code.includes('useCssVars as _useCssVars')).toBeTruthy()
+    expect(`'${hashId('testVCF' + 'color')}': (color.value)`).toBeTruthy()
+    expect(code).toMatchSnapshot()
+  })
+
+  test('Should be injected based on normal variables', () => {
+    const mockVCFNormal = mockVCF.replace('ref(\'red\')', '\'red\'')
+    const res = compileVineTypeScriptFile(mockVCFNormal, 'mockVCFNormal', mockCompilerHook)
+    const code = res.fileSourceCode.toString()
+    expect(code.includes('useCssVars as _useCssVars')).toBeTruthy()
+    expect(`'${hashId('testVCF' + 'color')}': (color)`).toBeTruthy()
+    expect(code).toMatchSnapshot()
+  })
+
+  test('Should be injected based on props', () => {
+    const mockVCFNormal = mockVCF.replace('ref(\'red\')', 'vineProp.withDefault(\'red\')')
+    const res = compileVineTypeScriptFile(mockVCFNormal, 'mockVCFNormal', mockCompilerHook)
+    const code = res.fileSourceCode.toString()
+    expect(code.includes('useCssVars as _useCssVars')).toBeTruthy()
+    expect(`'${hashId('testVCF' + 'color')}': (props.color)`).toBeTruthy()
+    expect(code).toMatchSnapshot()
+  })
+
+  test('Should be able to inject according to each subcomponent of vfc', () => {
+    const mockVCFSub = `${mockVCF}\nfunction testVCFS() {\n`
+      + '  const color = ref(\'red\')\n'
+      + '\n'
+      + '  vineStyle.scoped(`\n'
+      + '    .blog-title {\n'
+      + '      color: v-bind(color)\n'
+      + '    }\n'
+      + '  `)\n'
+      + '  return vine`\n'
+      + '     <div class="title-s">\n'
+      + '        title\n'
+      + '     </div>\n'
+      + '  `\n'
+      + '}'
+
+    const res = compileVineTypeScriptFile(mockVCFSub, 'mockVCFNormal', mockCompilerHook)
+    const code = res.fileSourceCode.toString()
+    expect(code.includes('useCssVars as _useCssVars')).toBeTruthy()
+    expect(`'${hashId('testVCF' + 'color')}': (color.value)`).toBeTruthy()
+    expect(`'${hashId('testVCFS' + 'color')}': (color.value)`).toBeTruthy()
+    expect(code).toMatchSnapshot()
+  })
+
+  test('Should work with w/ complex expression', () => {
+    const mockComplex = 'function testVCFComplex() {\n'
+      + '  let a = 100\n'
+      + '  let b = 200\n'
+      + '  let foo = 300\n'
+      + '\n'
+      + '  vineStyle.scoped(`\n'
+      + '     p {\n'
+      + '       width: calc(v-bind(foo) - 3px);\n'
+      + '       height: calc(v-bind(\'foo\') - 3px);\n'
+      + '       top: calc(v-bind(foo + \'px\') - 3px);\n'
+      + '     }\n'
+      + '     div {\n'
+      + '       color: v-bind((a + b) / 2 + \'px\' );\n'
+      + '     }\n'
+      + '     div {\n'
+      + '       color: v-bind    ((a + b) / 2 + \'px\' );\n'
+      + '     }\n'
+      + '     p {\n'
+      + '       color: v-bind(((a + b)) / (2 * a));\n'
+      + '     }\n'
+      + '  `)\n'
+      + '  return vine`\n'
+      + '     <div class="title">\n'
+      + '        title\n'
+      + '     </div>\n'
+      + '  `\n'
+      + '}\n'
+    const res = compileVineTypeScriptFile(mockComplex, 'testVCFComplex', mockCompilerHook)
+    const code = res.fileSourceCode.toString()
+    expect(code.includes('useCssVars as _useCssVars')).toBeTruthy()
+    expect(`'${hashId('testVCFComplex' + 'a')}': (a)`).toBeTruthy()
+    expect(`'${hashId('testVCFComplex' + 'b')}': (b)`).toBeTruthy()
+    expect(`'${hashId('testVCFComplex' + 'c')}': (c)`).toBeTruthy()
+    expect(code).toMatchSnapshot()
+  })
+})
