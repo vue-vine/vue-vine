5790ecbd2121453c1c735e083715cf7d85c7141e
so1ve <nn_201312@163.com>
2023-07-26 15:00:27 +0800

    chore: rename

--- scripts/ext-dev.js
+++ scripts/ext-dev.js
@@ -2,7 +2,7 @@ import { log, setGlobalPrefix } from '@baiwusanyu/utils-log'
 import { runCommand } from './utils'
 
 async function runExtScript(mode) {
-  const WATCH_COMPILER = 'pnpm --filter @vue-vine/compiler run build'
+  const BUILD_COMPILER = 'pnpm --filter @vue-vine/compiler run build'
   const WATCH_EXT_TSC = `pnpm --filter vue-vine-extension run ${mode}:tsc`
   const WATCH_EXT_ESBUILD = `pnpm --filter vue-vine-extension run ${mode}:esbuild`
   const WATCH_LANGUAGE_SERVER = `pnpm --filter @vue-vine/language-server run ${mode}`
@@ -11,7 +11,7 @@ async function runExtScript(mode) {
   setGlobalPrefix('[vue-vine]: ')
 
   try {
-    await runCommand(WATCH_COMPILER)
+    await runCommand(BUILD_COMPILER)
     await Promise.all([
       runCommand(WATCH_EXT_TSC),
       runCommand(WATCH_EXT_ESBUILD),
