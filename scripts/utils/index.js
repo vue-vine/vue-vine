60eeb85978bb181d070aebee44571dd104341cb1
ShenQingchuan <tmy991106@gmail.com>
2023-08-07 15:47:13 +0800

    chore: improve scripts

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -1,5 +1,5 @@
 import * as path from 'node:path'
-import { exec, spawn } from 'node:child_process'
+import { exec } from 'node:child_process'
 import { log } from '@baiwusanyu/utils-log'
 import { colorful } from './color-str'
 
@@ -29,38 +29,6 @@ export async function runCommand(command, options = {}) {
       })
       proc.on('SIGINT', () => {
         log('info', `Exit ${options.title} ...`)
-      })
-    }
-    catch (err) {
-      log('error', String(err), `${colorful(
-      `${options.title ? ` ${options.title} ` : ' '}`,
-        ['white', 'bgRed', 'bold'],
-      )}  `)
-      reject(err)
-    }
-  })
-}
-
-export async function spawnCommand(command, args = [], options = {}) {
-  options.title && log('info', '(spawn) Executing ...', `${colorful(
-  `${options.title ? ` ${options.title} ` : ' '}`,
-    ['black', 'bgBlue', 'bold'],
-  )}  `)
-
-  return new Promise((resolve, reject) => {
-    try {
-      const proc = spawn(command, args, {
-        cwd: r('../'),
-        shell: true,
-        stdio: 'inherit',
-      })
-      proc.on('close', resolve)
-      proc.on('exit', resolve)
-      proc.on('error', (err) => {
-        log('error', String(err), `${colorful(
-        `${options.title ? ` ${options.title} ` : ' '}`,
-          ['black', 'bgRed', 'bold'],
-        )}  `)
       })
     }
     catch (err) {
