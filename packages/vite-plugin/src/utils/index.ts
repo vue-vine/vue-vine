a858e9c33af02f8d35a005838769d5cd73fbf8e1
Ray <i@mk1.io>
2023-09-11 12:02:35 +0800

    Update index.ts

--- packages/vite-plugin/src/utils/index.ts
+++ packages/vite-plugin/src/utils/index.ts
@@ -2,8 +2,7 @@ export function areStrArraysEqual(arr1: string[], arr2: string[]) {
   const set1 = new Set(arr1)
   const set2 = new Set(arr2)
 
-  return set1.size === set2.size
-    && [...set1].every(item => set2.has(item))
+  return set1.size === set2.size && [...set1].every(item => set2.has(item))
 }
 
 export function normalizeLineEndings(str: string) {
