385cecb69939bbef26820c05d515a10e7de0cd25
Alex <alexzhang1030@foxmail.com>
2024-04-23 10:47:35 +0800

    docs(style): prettify recommendations

--- packages/docs/src/.vitepress/theme/components/recommend.vue
+++ packages/docs/src/.vitepress/theme/components/recommend.vue
@@ -7,14 +7,23 @@
     <div
       class="
         w-fit
-        flex flex-row justify-center items-center py-4
+        flex flex-row justify-center items-center p-4
         bg-gray-100/50 dark:bg-gray-800/50
-        rounded-lg shadow-lg mt-4
+        rounded-lg mt-4 gap-2
       "
     >
-      <div class="i-logos-vitejs text-3xl mx-4" />
-      <div class="i-logos-unocss text-3xl mx-4 light:brightness-9 dark:filter-invert-90" />
-      <div class="i-logos-visual-studio-code text-3xl mx-4" />
+      <div class="w-fit flex py2 relative group">
+        <div class="group-hover:(bg-gradient-to-tl from-[#bd34fe_50%] to-[#47caff_50%]) recommendation-bg" />
+        <a class="i-logos-vitejs text-3xl cursor-pointer z-2 mx4" href="https://vitejs.dev/" target="_blank" />
+      </div>
+      <div class="w-fit flex py2 relative group">
+        <div class="group-hover:bg-gray-500/50 recommendation-bg" />
+        <a class="i-logos-unocss text-3xl cursor-pointer z-2 mx4 light:brightness-9 dark:filter-invert-90" href="https://unocss.dev/" target="_blank" />
+      </div>
+      <div class="w-fit flex py2 relative group">
+        <div class="group-hover:(bg-gradient-to-tl to-[#4D9DE3_50%] from-[#69ACFF_50%]) recommendation-bg" />
+        <a class="i-logos-visual-studio-code text-3xl cursor-pointer z-2 mx4" href="https://code.visualstudio.com/" target="_blank" />
+      </div>
     </div>
   </div>
 </template>
