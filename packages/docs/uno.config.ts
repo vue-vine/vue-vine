dd5e23b5aa5d0a1bfd4477093c390d777d19926a
Alex <49969959+alexzhang1030@users.noreply.github.com>
2024-04-25 22:01:08 +0800

    docs(style): prettify recommendations (#78)

--- packages/docs/uno.config.ts
+++ packages/docs/uno.config.ts
@@ -1,5 +1,5 @@
 // uno.config.ts
-import { defineConfig, presetIcons, presetUno } from 'unocss'
+import { defineConfig, presetIcons, presetUno, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
   presets: [
@@ -10,4 +10,10 @@ export default defineConfig({
       unit: 'em',
     }),
   ],
+  transformers: [
+    transformerVariantGroup(),
+  ],
+  shortcuts: {
+    'recommendation-bg': 'group-hover:(blur-md op100) op0 transition-opacity top-10% left-15% absolute rounded-50% w-75% h-75%',
+  },
 })
