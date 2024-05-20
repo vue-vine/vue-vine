38078f557acc3ad98c1c0fd7c55cfcd74d09dfe5
Alex <alexzhang1030@foxmail.com>
2024-05-20 14:54:24 +0800

    fix: test script

--- scripts/run-test.js
+++ scripts/run-test.js
@@ -3,6 +3,6 @@ import { runCommand } from './utils'
 const testCommand = 'cross-env NODE_ENV=test '
   + 'pnpm run test:compiler --run && '
   + 'pnpm run test:e2e --run && '
-  + 'pnpm run test:eslint-parser --run'
+  + 'pnpm run test:eslint-parser'
 
 runCommand(testCommand)
