b29ab1c1fded436a2f0a71a03262228758441145
ShenQingchuan <tmy991106@gmail.com>
2024-07-05 15:46:04 +0800

    fix(macros): macros type declares

--- packages/vue-vine/types/macros.d.ts
+++ packages/vue-vine/types/macros.d.ts
@@ -3,42 +3,42 @@ import type { Ref } from 'vue'
 declare const __VUE_VINE__: unique symbol
 declare const __VUE_VINE_STYLE__: unique symbol
 
-declare interface VueVineComponent { [__VUE_VINE__]: true }
-declare interface VineStyle { [__VUE_VINE_STYLE__]: true }
-
 type IsEmptyObj<T> = keyof T extends never ? true : false
 
-type VineEmitsDefineSource = Record<string, any[]>
-type VineEmitsDefineResult<D extends VineEmitsDefineSource> = IsEmptyObj<D> extends true
-  ? never
-  : <E extends keyof D>(
-  eventName: E,
-  ...args: D[E]
-) => void
-
-type VinePropValidator<T> = (value: T) => boolean
-interface VinePropMacro {
-  <T>(validator?: VinePropValidator<T>): Readonly<Ref<T>>
-  optional: <T>(validator?: VinePropValidator<T>) => Readonly<Ref<T>>
-  withDefault: <T>(value: T, validator?: VinePropValidator<T>) => Readonly<Ref<T>>
-}
-
-interface VineStyleMacro {
-  (style: string | VineStyle): void
-  scoped: (style: string | VineStyle) => void
-}
-
-interface VineOptionsDef {
-  name?: string
-  inheritAttrs?: boolean
-}
-
-interface VineEmitsFn {
-  <E extends string>(events: E[]): VineEmitsDefineResult<Record<E, any[]>>
-  <D extends VineEmitsDefineSource>(): VineEmitsDefineResult<D>
-}
-
 declare global {
+  interface VueVineComponent { [__VUE_VINE__]: true }
+  interface VineStyle { [__VUE_VINE_STYLE__]: true }
+
+  type VineEmitsDefineSource = Record<string, any[]>
+  type VineEmitsDefineResult<D extends VineEmitsDefineSource> = IsEmptyObj<D> extends true
+    ? never
+    : <E extends keyof D>(
+    eventName: E,
+    ...args: D[E]
+  ) => void
+
+  type VinePropValidator<T> = (value: T) => boolean
+  interface VinePropMacro {
+    <T>(validator?: VinePropValidator<T>): Readonly<Ref<T>>
+    optional: <T>(validator?: VinePropValidator<T>) => Readonly<Ref<T>>
+    withDefault: <T>(value: T, validator?: VinePropValidator<T>) => Readonly<Ref<T>>
+  }
+
+  interface VineStyleMacro {
+    (style: string | VineStyle): void
+    scoped: (style: string | VineStyle) => void
+  }
+
+  interface VineOptionsDef {
+    name?: string
+    inheritAttrs?: boolean
+  }
+
+  interface VineEmitsFn {
+    <E extends string>(events: E[]): VineEmitsDefineResult<Record<E, any[]>>
+    <D extends VineEmitsDefineSource>(): VineEmitsDefineResult<D>
+  }
+
   const vineProp: VinePropMacro
 
   const vineEmits: VineEmitsFn
