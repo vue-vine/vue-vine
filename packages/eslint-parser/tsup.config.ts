c15323a5e4d41b5835a307415babea68084b8c58
ShenQingchuan <tmy991106@gmail.com>
2024-07-08 00:19:06 +0800

    chore(eslint-parser): remove external for bundle config

--- packages/eslint-parser/tsup.config.ts
+++ packages/eslint-parser/tsup.config.ts
@@ -3,15 +3,4 @@ import base from '../../tsup.config'
 
 export default defineConfig({
   ...base,
-  external: [
-    '@babel/types',
-    '@babel/parser',
-    'eslint-scope',
-    'espree',
-    'line-column',
-    'semver',
-    '@typescript-eslint/parser',
-    '@typescript-eslint/scope-manager',
-    '@typescript-eslint/typescript-estree',
-  ],
 })
