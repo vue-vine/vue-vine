fa86e5c123310f73a0453e6e0ccf36590ace8157
ShenQingchuan <tmy991106@gmail.com>
2024-08-27 03:28:04 +0800

    chore: fix repo ESLint config [ci skip]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,9 @@
+import { createRequire } from 'node:module'
 import antfu from '@antfu/eslint-config'
 import * as VueVineESLintParser from '@vue-vine/eslint-parser'
+
+const require = createRequire(import.meta.url)
+const VueVineESLintParserPackageJSON = require('@vue-vine/eslint-parser/package.json')
 
 export default antfu(
   {
@@ -28,7 +32,13 @@ export default antfu(
       'packages/nuxt-module/playground/**/*.vine.ts',
     ],
     languageOptions: {
-      parser: VueVineESLintParser,
+      parser: {
+        meta: {
+          name: '@vue-vine/eslint-parser',
+          version: VueVineESLintParserPackageJSON.version,
+        },
+        ...VueVineESLintParser,
+      },
     },
     rules: {
       'no-console': 'off',
