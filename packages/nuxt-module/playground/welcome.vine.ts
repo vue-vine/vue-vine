ea914fb3db3187c352bdf6a255e76f709fc06208
wzc520pyfm <1528857653@qq.com>
2024-08-27 22:37:55 +0800

    chore(nuxt-module): add more vine define of playground

--- packages/nuxt-module/playground/welcome.vine.ts
+++ packages/nuxt-module/playground/welcome.vine.ts
@@ -1,11 +1,23 @@
 function Welcome() {
   const content = vineProp<string>()
   const counter = ref(0)
+  const contentBgColor = ref('red')
+
+  vineStyle.scoped(scss`
+    .content-view {
+      margin: 1rem 0;
+      color: v-bind(contentBgColor);
+    }
+  `)
+
+  vineExpose({
+    contentBgColor,
+  })
 
   return vine`
     <div>
       <div>Vue Vine!</div>
-      <div>{{ content }}</div>
+      <div class="content-view">{{ content }}</div>
       <div>
         <button @click="() => counter++">+</button>
         {{ counter }}
