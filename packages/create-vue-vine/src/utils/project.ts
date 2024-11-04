4b3d6a9f25bd025f1297e2969ddf94376b9b10af
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-11-04 22:32:13 +0800

    fix(create-vue-vine): get correct template dir in `Windows`

--- packages/create-vue-vine/src/utils/project.ts
+++ packages/create-vue-vine/src/utils/project.ts
@@ -1,11 +1,12 @@
 import { basename, resolve } from 'node:path'
 import { exists } from './fs'
+import { fileURLToPath } from 'node:url'
 
 export function validateProjectName(path: string) {
   return basename(path) === path
 }
 
-const __dirname = new URL('.', import.meta.url).pathname
+const __dirname = fileURLToPath(import.meta.url)
 
 export async function getTemplateDirectory() {
   const templateRoot = resolve(__dirname, '../template')
