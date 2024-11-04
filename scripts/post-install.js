06949813295de3037f407d112f2763e13700c769
ShenQingchuan <tmy991106@gmail.com>
2024-11-05 01:07:03 +0800

    chore: update postinstall script due to netlify broken

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises'
 import { resolve } from 'node:path'
+import process from 'node:process'
 import url from 'node:url'
 import { log } from '@baiwusanyu/utils-log'
 import { cliExec } from './utils'
@@ -7,6 +8,10 @@ import { cliExec } from './utils'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 async function buildESLintParser() {
+  if (typeof process.stdin.setRawMode !== 'function') {
+    return
+  }
+
   const eslintParserDistDir = resolve(__dirname, '../packages/eslint-parser/dist')
   const isESLintParserDistDirExists = await fs.stat(eslintParserDistDir).then(() => true).catch(() => false)
 
