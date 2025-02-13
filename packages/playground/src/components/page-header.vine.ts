24543b9e6ccd48a82f05f1af313dbc2ceab9308c
ShenQingchuan <tmy991106@gmail.com>
2025-02-13 12:08:36 +0800

    wip: refactor nav btn in playground

--- packages/playground/src/components/page-header.vine.ts
+++ packages/playground/src/components/page-header.vine.ts
@@ -1,9 +1,10 @@
 import { useRoute, useRouter } from 'vue-router'
 
-export function PageHeader() {
+function NavBtn(props: {
+  path: string
+}) {
   const route = useRoute()
   const router = useRouter()
-
   const handleNavBtnClick = (
     target: string,
   ) => {
@@ -16,44 +17,27 @@ export function PageHeader() {
 
   return vine`
     <div
+      class="ml-2 px-4 py-2 rounded whitespace-nowrap bg-teal-700:20 dark:bg-coolgray-100:20 cursor-pointer"
+      @click="handleNavBtnClick(path)"
+    >
+      <span>
+        {{ route.path === path ? "Go Home" : path.slice(1) }}
+      </span>
+    </div>
+  `
+}
+
+export function PageHeader() {
+  return vine`
+    <div
       class="w-full px-4 py-6 border-b-1px border-b-solid border-b-coolgray-300:50 dark:border-b-coolgray-100:20 flex items-center fixed top-0"
     >
-      <h2>Vine playground</h2>
-      <div
-        class="ml-2 px-4 py-2 rounded whitespace-nowrap bg-teal-700:20 dark:bg-coolgray-100:20 cursor-pointer"
-        @click="handleNavBtnClick('/style-order')"
-      >
-        <span>
-          {{ route.path === "/style-order" ? "Go Home" : "TestStyleOrder" }}
-        </span>
-      </div>
-
-      <div
-        class="ml-2 px-4 py-2 rounded whitespace-nowrap bg-teal-700:20 dark:bg-coolgray-100:20 cursor-pointer"
-        @click="handleNavBtnClick('/about')"
-      >
-        <span>
-          {{ route.path === "/about" ? "Go Home" : "About" }}
-        </span>
-      </div>
-      <div
-        class="ml-2 px-4 py-2 rounded whitespace-nowrap bg-teal-700:20 dark:bg-coolgray-100:20 cursor-pointer"
-        @click="handleNavBtnClick('/todolist')"
-      >
-        <span> TodoList </span>
-      </div>
-      <div
-        class="ml-2 px-4 py-2 rounded whitespace-nowrap bg-teal-700:20 dark:bg-coolgray-100:20 cursor-pointer"
-        @click="handleNavBtnClick('/test-ts-morph')"
-      >
-        <span> ts morph </span>
-      </div>
-      <div
-        class="ml-2 px-4 py-2 rounded whitespace-nowrap bg-teal-700:20 dark:bg-coolgray-100:20 cursor-pointer"
-        @click="handleNavBtnClick('/vapor')"
-      >
-        <span> Vapor </span>
-      </div>
+      <h2 class="mr-4">Vine playground</h2>
+      <NavBtn path="/style-order" />
+      <NavBtn path="/about" />
+      <NavBtn path="/todolist" />
+      <NavBtn path="/test-ts-morph" />
+      <NavBtn path="/vapor" />
     </div>
   `
 }
