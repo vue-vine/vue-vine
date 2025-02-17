e764e428c7e4b8782aec4a71dbb4d5aba6229cc5
AngelHdz <angel.hernandez.12@live.com>
2025-02-17 15:58:29 -0400

    fix(nuxt-module): Fixes nuxt pages

--- packages/nuxt-module/src/module.ts
+++ packages/nuxt-module/src/module.ts
@@ -1,9 +1,35 @@
+import type { NuxtPage } from '@nuxt/schema'
 import type { PluginOption } from 'vite'
 import { defineNuxtModule } from '@nuxt/kit'
 import { VineVitePlugin } from 'vue-vine/vite'
+import { consola } from 'consola'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
+
+function endsWith(searchString: string | undefined, extension: string, suffix?: string) {
+  return searchString?.endsWith(`.${suffix ? `${suffix}.` : '' }${extension}`)
+}
+
+function plur(count: number, plural: string, singular: string = '') {
+  return count === 1 ? singular : plural
+}
+
+function addPages(pages: NuxtPage[]) {
+  const count = {sfc: 0, vine: 0} 
+  for (const page of pages) {
+    console.log(page.file)
+    if ( endsWith(page.file, 'ts', 'vine')) {
+      page.path = page.path.replace('.vine', '')
+      page.path = page.path === '/index' ? '/' : page.path
+
+      count.vine += 1
+    } else if (endsWith(page.file, 'vue')) {
+      count.sfc += 1
+    }
+  }
+  consola.success(`Added ${count.vine} Vue Vine page${plur(count.vine, 's')}, ${count.sfc} SFC page${plur(count.sfc, 's')}.`)
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -29,5 +55,7 @@ export default defineNuxtModule<ModuleOptions>({
       config.plugins = config.plugins || []
       config.plugins.unshift(VineVitePlugin() as PluginOption)
     })
+
+    _nuxt.hook('pages:extend', (pages) => addPages(pages))
   },
 })
