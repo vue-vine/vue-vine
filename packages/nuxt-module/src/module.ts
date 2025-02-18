6e759898a2b470c868b783543f1695d4a60f207c
AngelHdz <angel.hernandez.12@live.com>
2025-02-18 01:14:41 -0400

    fix(nuxt-module): Fixes nuxt pages (#211)
    * Update package.json
    Added consola as part of the `fix(nuxt-module): Fixes nuxt pages`  pr.
    * fix(nuxt-module): Fixes nuxt pages
    * fix: apply some lint and improve printed log
    * chore: update packages meta for github repo
    ---------
    Co-authored-by: ShenQingchuan <tmy991106@gmail.com>

--- packages/nuxt-module/src/module.ts
+++ packages/nuxt-module/src/module.ts
@@ -1,9 +1,37 @@
+import type { NuxtPage } from '@nuxt/schema'
 import type { PluginOption } from 'vite'
 import { defineNuxtModule } from '@nuxt/kit'
+import { consola } from 'consola'
+import { colorize } from 'consola/utils'
 import { VineVitePlugin } from 'vue-vine/vite'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  // ...
+}
+
+function plur(count: number, plural: string, singular: string = '') {
+  return count <= 1 ? singular : plural
+}
+
+function addPages(pages: NuxtPage[]) {
+  const count = { sfc: 0, vine: 0 }
+  for (const page of pages) {
+    if (page.file?.endsWith('.vine.ts')) {
+      page.path = page.path.replace('.vine', '')
+      page.path = page.path === '/index' ? '/' : page.path
+
+      count.vine += 1
+    }
+    else if (page.file?.endsWith('.vue')) {
+      count.sfc += 1
+    }
+  }
+  consola.success(`
+  Added ${count.vine} page${plur(count.vine, 's')} by ${colorize('blue', 'Vine (.vine.ts)')},
+  Added ${count.sfc} page${plur(count.sfc, 's')} by ${colorize('green', 'SFC (.vue)')}
+  `.trim())
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -29,5 +57,7 @@ export default defineNuxtModule<ModuleOptions>({
       config.plugins = config.plugins || []
       config.plugins.unshift(VineVitePlugin() as PluginOption)
     })
+
+    _nuxt.hook('pages:extend', pages => addPages(pages))
   },
 })
