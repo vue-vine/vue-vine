68e57b3292d639ce45faedf447cadde806b4b59b
Libon <bon.li@outlook.com>
2025-03-30 14:07:40 +0800

    feat(create-vue-vine): query the latest version to replace the template version

--- packages/create-vue-vine/src/utils/renderTemplate.ts
+++ packages/create-vue-vine/src/utils/renderTemplate.ts
@@ -33,9 +33,62 @@ export async function renderTemplate(src: string, dest: string) {
     const existing = JSON.parse(fs.readFileSync(dest, 'utf8'))
     const newPackage = JSON.parse(fs.readFileSync(src, 'utf8'))
     const pkg = sortDependencies(deepMerge(existing, newPackage))
+
+    // vue-vine will only appear once,
+    // so you don't need to query the coverage every time.
+    if (src.endsWith('common/package.json')) {
+      await setLatestVineVersion(pkg, 'vue-vine')
+    }
+    else if (src.endsWith('ts/package.json')) {
+      await setLatestVineVersion(pkg, 'vue-vine-tsc')
+    }
+
     fs.writeFileSync(dest, `${JSON.stringify(pkg, null, 2)}\n`)
     return
   }
 
   await copyFile(src, dest)
+}
+
+let isLastFetchTimeout = false
+
+async function getLatestVineVersion(dep: string) {
+  // If the last query has timed out,
+  // all subsequent queries will not go through the server.
+  if (isLastFetchTimeout) {
+    return
+  }
+
+  const controller = new AbortController()
+
+  // If no response is received within 3 seconds, cancel the query,
+  // Avoid some areas where the network is too bad to access services.
+  const timeout = setTimeout(() => {
+    isLastFetchTimeout = true
+    controller.abort()
+  }, 3000)
+
+  try {
+    const res = await fetch(`https://npm.antfu.dev/${dep}@latest`, {
+      signal: controller.signal,
+    })
+    const pkg = await res.json()
+    return pkg.version
+  }
+  finally {
+    clearTimeout(timeout)
+  }
+}
+
+async function setLatestVineVersion(pkg: Record<string, any>, dep: string) {
+  try {
+    const latestVersion = await getLatestVineVersion(dep)
+
+    if (!latestVersion) {
+      return
+    }
+
+    pkg.devDependencies[dep] = `^${latestVersion}`
+  }
+  catch { }
 }
