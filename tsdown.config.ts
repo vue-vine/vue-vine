93b7a242b259c8d00fcf5d3225262ad4c0596432
ShenQingchuan <tmy991106@gmail.com>
2025-05-14 13:15:54 +0800

    wip: add dts: true back

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,3 +1,4 @@
+import { join } from 'node:path'
 import process from 'node:process'
 import { defineConfig } from 'tsdown'
 
@@ -8,6 +9,8 @@ export default defineConfig({
     include: ['packages/*'],
     exclude: ['packages/docs', 'packages/e2e-test', 'packages/nuxt-module', 'packages/playground'],
   },
+  dts: true,
+  tsconfig: join(import.meta.dirname, 'tsconfig.json'),
   entry: ['src/index.ts'],
   sourcemap: isDev,
 })
