8b28e3a009c73e7ec00c8b5dace22ecc3bf1cd0c
ShenQingchuan <tmy991106@gmail.com>
2025-05-18 12:48:11 +0800

    chore: add watch ext script

--- packages/playground/src/pages/home.vine.ts
+++ packages/playground/src/pages/home.vine.ts
@@ -41,7 +41,7 @@ function OutsideExample(props: { id: string }) {
         Random string:
       </div>
 
-      <div v-if="loading" class="loading-view" @click="">
+      <div v-if="loading" class="loading-view">
         <div class="icon-loading" />
         <span>Loading...</span>
       </div>
