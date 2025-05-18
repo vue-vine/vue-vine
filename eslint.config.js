a1687816bd4059cf403770162c6b46bd6ccfc7a0
ShenQingchuan <tmy991106@gmail.com>
2025-05-18 16:30:22 +0800

    chore: enable eslint pnpm option

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    pnpm: true,
     ignores: [
       'node_modules',
       'dist',
@@ -9,8 +10,8 @@ export default antfu(
 
       'packages/docs/.vitepress/cache',
       'packages/e2e-test/**/*.vine.ts',
-      'packages/playground/**/*.vine.ts',
-      'packages/create-vue-vine/template/**/*.vine.[j|t]s',
+      'packages/playground',
+      'packages/create-vue-vine/template/**/*',
       'packages/nuxt-module/playground/*',
     ],
   },
