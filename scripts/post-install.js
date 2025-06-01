6cebfb68bcbddd5a183e949f609c7d64a106a553
ShenQingchuan <tmy991106@gmail.com>
2025-06-01 21:28:07 +0800

    chore: update post install script

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 import process from 'node:process'
 import { log } from '@baiwusanyu/utils-log'
-import { cliExec, r } from './utils'
+import { runCommand, r } from './utils'
 
 async function buildESLintParser() {
   if (typeof process.stdin.setRawMode !== 'function') {
@@ -26,7 +26,7 @@ async function buildESLintParser() {
     return
   }
 
-  cliExec('pnpm run build:eslint')
+  runCommand('pnpm run build:eslint')
 }
 
 buildESLintParser()
