fe37a5ed1e58de7fe1d7b494fb24dc0c6ee5c973
liangmiQwQ <mi_yyds@126.com>
2025-07-29 07:53:54 +0800

    refactor(create-vue-vine): formatted app.vine.ts with ESLint

--- packages/create-vue-vine/template/code/router/src/app.vine.ts
+++ packages/create-vue-vine/template/code/router/src/app.vine.ts
@@ -8,13 +8,11 @@ export function App() {
   `)
 
   return vine`
-  <div class="max-w-screen-lg mx-auto h-full px-12 py-24">
-    <header class="header flex justify-between items-center pb-4">
-      <h1 class="font-medium text-lg">Vue Vine</h1>
+    <div class="max-w-screen-lg mx-auto h-full px-12 py-24">
+      <header class="header flex justify-between items-center pb-4">
+        <h1 class="font-medium text-lg">Vue Vine</h1>
 
-      <nav
-        class="nav-menu flex gap-2 font-medium font-sans text-sm tracking-tight"
-      >
+        <nav class="nav-menu flex gap-2 font-medium font-sans text-sm tracking-tight">
           <router-link
             class="px-4 py-1 transition-colors rounded-md hover:bg-[#88888828] active:bg-[#88888850]"
             active-class="text-[#3451b2] dark:text-[#5777e2]"
@@ -40,9 +38,7 @@ export function App() {
             <img :src="VineLogo" alt="Vine Logo" width="128" height="128" />
           </div>
           <h2 class="font-medium my-2">Another style to write Vue</h2>
-          <p class="text-sm">
-            Provide more flexibility for writing Vue components.
-          </p>
+          <p class="text-sm">Provide more flexibility for writing Vue components.</p>
         </div>
 
         <div class="content flex-1 p-4">
@@ -50,5 +46,5 @@ export function App() {
         </div>
       </main>
     </div>
-`
+  `
 }
