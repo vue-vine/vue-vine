f94e29cf404172d51e19cb3192c9dceed79f727c
Ray <i@mk1.io>
2025-12-19 19:28:30 +0800

    feat(create-vue-vine): migrate to clerc v1 (#346)
    * feat: migrate to clerc v1
    
    * chore: install tsgo
    
    * chore: update clerc
    
    * chore: bump clerc
    
    * chore: bump clerc
    
    ---------
    
    Co-authored-by: ShenQingchuan <tmy991106@gmail.com>

--- packages/create-vue-vine/src/commands/create.ts
+++ packages/create-vue-vine/src/commands/create.ts
@@ -3,7 +3,7 @@ import { rm } from 'node:fs/promises'
 import { join } from 'node:path'
 import process from 'node:process'
 import { intro, log, outro, spinner } from '@clack/prompts'
-import { defineCommand, Root } from 'clerc'
+import { defineCommand } from 'clerc'
 import { bold, green } from 'yoctocolors'
 import { createProject, createProjectOptions } from '../create'
 import { useFlags } from '../flags'
@@ -11,100 +11,100 @@ import { cancel, confirm, exists, formatPmCommand, getPmCommand, getTemplateDire
 
 const { flags, executeFlags } = useFlags()
 const defaultProjectName = 'vue-vine-project'
-const parameters: ['[projectName]'] = ['[projectName]']
 
-export const createCommand: Command<typeof Root, {
-  parameters: typeof parameters
-}> = defineCommand({
-  name: Root,
-  description: 'Create a Vue Vine project',
-  parameters,
-  flags: {
-    force: {
-      type: Boolean,
-      description: 'Delete existing folder',
-      alias: 'f',
-      default: false,
-    },
-    ...flags,
-  },
-  alias: 'create',
-}, async (ctx) => {
-  intro(gradientBanner)
-
-  // Check whether the template exists first,
-  // otherwise everything is meaningless.
-  const templateDir = await getTemplateDirectory()
-  if (!templateDir) {
-    cancel('Unable to find template directory')
-  }
-
-  let projectName = ctx.parameters.projectName || defaultProjectName
-
-  if (!ctx.parameters.projectName) {
-    projectName = await text({
-      message: 'Project name:',
-      placeholder: defaultProjectName,
-      defaultValue: defaultProjectName,
-      validate: (value) => {
-        if (!validateProjectName(value)) {
-          return 'Invalid project name'
-        }
+export const createCommand: Command = defineCommand(
+  {
+    name: '',
+    description: 'Create a Vue Vine project',
+    parameters: ['[project name]'],
+    flags: {
+      force: {
+        type: Boolean,
+        description: 'Delete existing folder',
+        alias: 'f',
+        default: false,
       },
-    })
-  }
+      ...flags,
+    },
+    alias: 'create',
+  },
+  async (ctx) => {
+    intro(gradientBanner)
 
-  const cwd = process.cwd()
-  const projectPath = join(cwd, projectName)
+    // Check whether the template exists first,
+    // otherwise everything is meaningless.
+    const templateDir = await getTemplateDirectory()
+    if (!templateDir) {
+      cancel('Unable to find template directory')
+    }
 
-  const isSameProjectExists = await exists(projectPath)
-  if (isSameProjectExists) {
-    if (!ctx.flags.force) {
-      ctx.flags.force = await confirm({
-        message: `Folder ${projectName} already exists. Delete?`,
-        initialValue: false,
+    let projectName = ctx.parameters.projectName || defaultProjectName
+
+    if (!ctx.parameters.projectName) {
+      projectName = await text({
+        message: 'Project name:',
+        placeholder: defaultProjectName,
+        defaultValue: defaultProjectName,
+        validate: (value) => {
+          if (!validateProjectName(value)) {
+            return 'Invalid project name'
+          }
+        },
       })
     }
 
-    if (!ctx.flags.force) {
-      cancel(`Folder ${projectName} already exists. Goodbye!`)
+    const cwd = process.cwd()
+    const projectPath = join(cwd, projectName)
+
+    const isSameProjectExists = await exists(projectPath)
+    if (isSameProjectExists) {
+      if (!ctx.flags.force) {
+        ctx.flags.force = await confirm({
+          message: `Folder ${projectName} already exists. Delete?`,
+          initialValue: false,
+        })
+      }
+
+      if (!ctx.flags.force) {
+        cancel(`Folder ${projectName} already exists. Goodbye!`)
+      }
+      else {
+        log.info(`Folder ${projectName} will be deleted.`)
+        await rm(projectPath, { recursive: true })
+      }
     }
-    else {
-      log.info(`Folder ${projectName} will be deleted.`)
-      await rm(projectPath, { recursive: true })
+
+    const projectOptions = createProjectOptions({
+      path: projectPath,
+      name: projectName,
+      templateDir,
+      buildTool: 'vite', // Will be overwritten by executeFlags
+    })
+
+    await executeFlags(ctx.flags, projectOptions)
+
+    const s = spinner()
+    s.start(`Creating project ${projectName}`)
+    await createProject(projectOptions)
+    s.stop(`Project created at: ${projectPath}`)
+
+    if (ctx.flags.install) {
+      s.start('Installing dependencies')
+      await runPmCommand('install', projectPath)
+      s.stop('Dependencies installed!')
     }
-  }
 
-  const projectOptions = createProjectOptions({
-    path: projectPath,
-    name: projectName,
-    templateDir,
-    buildTool: 'vite', // Will be overwritten by executeFlags
-  })
+    const helpText = [
+      'You\'re all set! Now run:',
+      '',
+      `   cd ${bold(green(projectName.includes(' ') ? `"${projectName}"` : projectName))}`,
+      ctx.flags.install ? undefined : `   ${bold(green(formatPmCommand(getPmCommand('install'))))}`,
+      `   ${bold(green(formatPmCommand(getPmCommand('dev'))))}`,
+      '',
+      '   Happy hacking!',
+    ].filter(s => s !== undefined).join('\n')
 
-  await executeFlags(ctx.flags, projectOptions)
-
-  const s = spinner()
-  s.start(`Creating project ${projectName}`)
-  await createProject(projectOptions)
-  s.stop(`Project created at: ${projectPath}`)
-
-  if (ctx.flags.install) {
-    s.start('Installing dependencies')
-    await runPmCommand('install', projectPath)
-    s.stop('Dependencies installed!')
-  }
-
-  const helpText = [
-    'You\'re all set! Now run:',
-    '',
-    `   cd ${bold(green(projectName.includes(' ') ? `"${projectName}"` : projectName))}`,
-    ctx.flags.install ? undefined : `   ${bold(green(formatPmCommand(getPmCommand('install'))))}`,
-    `   ${bold(green(formatPmCommand(getPmCommand('dev'))))}`,
-    '',
-    '   Happy hacking!',
-  ].filter(s => s !== undefined).join('\n')
-
-  outro(helpText)
-  process.exit() // Ugh, why
-})
+    outro(helpText)
+    process.exit() // Ugh, why
+  },
+)
